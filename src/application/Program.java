09739e5979175aa65e30d1c6d7ae48124d472911
Pedro Carvalho <pedro8.carvalho@gmail.com>
2025-01-18 10:23:37 -0300

    new comment

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,7 @@ public class Program {
 			
 			for (Candidato candidato: candidatos.keySet()) {
 				System.out.printf("Candidato %s: %d%n", candidato.getNome(), candidatos.get(candidato));
+				//new comment
 			}
 		} 
 		
