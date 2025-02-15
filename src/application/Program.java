dfe4c2a3afa65bc8ff6ca5cef26dbc219ba98972
Pedro Carvalho <pedro8.carvalho@gmail.com>
2025-02-15 11:02:38 -0300

    new program

---
+++ src/application/Program.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import entities.Candidato;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter file full path: ");
+		String filePath = sc.nextLine();
+		
+		Map<Candidato, Integer> candidatos = new HashMap<>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] info = line.split(",");
+				String nome = info[0];
+				Integer votos = Integer.parseInt(info[1]);
+				if (candidatos.containsKey(new Candidato(nome))) {
+					candidatos.put(new Candidato(nome), candidatos.get(new Candidato(nome)) + votos);
+				}
+				else {
+					candidatos.put(new Candidato(nome), votos);
+				}
+				line = br.readLine();
+			}
+			
+			for (Candidato candidato: candidatos.keySet()) {
+				System.out.printf("Candidato %s: %d%n", candidato.getNome(), candidatos.get(candidato));
+				//new comment
+				//second comment
+				//third comment
+			}
+		} 
+		
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		sc.close();
+	}
+
+}
---
+++ src/entities/Candidato.java
@@ -0,0 +1,38 @@
+package entities;
+
+import java.util.Objects;
+
+public class Candidato {
+	private String nome;
+
+	public Candidato(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Candidato other = (Candidato) obj;
+		return Objects.equals(nome, other.nome);
+	}
+	
+	
+}
